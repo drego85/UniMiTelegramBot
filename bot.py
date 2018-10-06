7e49fb6c1fa1c6b42457107d9e582b858d3ab987
Andre Draghetti <drego85@draghetti.it>
2018-10-06 11:52:10 +0200

    Fix gestione utente senza username

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ def handle(msg):
         logging.info("%s: %s" % (content_type, msg["from"]["first_name"]))
 
     if content_type == "new_chat_member" and chat_id in Config.chatidList:
-        if msg["from"]["username"]:
+        if "username" in msg["from"]:
             bot.sendMessage(chat_id,
                             "Benvenuto @%s, ti ricordiamo che questo è il gruppo dedicato agli studenti ONLINE di SSRI. *Ti preghiamo di leggere* [questo messaggio](https://t.me/canalessri/86) *poichè contiene informazioni importanti e sfrutta la ricerca prima di fare delle domande*. Grazie 😉" %
                             msg["from"]["username"], parse_mode="Markdown", disable_web_page_preview=True)
