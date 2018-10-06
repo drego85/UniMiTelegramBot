f2f69141e843d590609e2a2c9f99f4bf9a89e1da
Andre Draghetti <drego85@draghetti.it>
2018-10-06 09:58:52 +0200

    Fix

--- bot.py
+++ bot.py
@@ -30,8 +30,8 @@ def handle(msg):
         else:
             bot.sendMessage(chat_id,
                             "Benvenuto @%s, ti ricordiamo che questo è il gruppo dedicato agli studenti ONLINE di SSRI. *Ti preghiamo di leggere* [questo messaggio](https://t.me/canalessri/86) *poichè contiene informazioni importanti e sfrutta la ricerca prima di fare delle domande*. Grazie 😉" %
-                            msg["from"]["first_name"], parse_mode="Markdown")
-            logging.info("%s: %s" % (content_type, msg["from"]["first_name"]), disable_web_page_preview=True)
+                            msg["from"]["first_name"], parse_mode="Markdown", disable_web_page_preview=True)
+            logging.info("%s: %s" % (content_type, msg["from"]["first_name"]))
 
 
 try:
